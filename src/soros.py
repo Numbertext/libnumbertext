ebd8d07a9da59632584287d5ea32ac034ce6e30c
László Németh <nemeth@numbertext.org>
2018-04-04 11:23:29 +0200

    import Numbertext 0.8.1 (2010-04-22)

--- src/soros.py
+++ src/soros.py
@@ -28,7 +28,7 @@ if output != "":
 
 if len(params) < 2:
     print "Usage: soros [-o file_output] [-p prefix] soros_file number(s)"
-    print "numbers may be ranges: eg. 1-100"
+    print "numbers may be ranges (1-100) or ranges with steps (1-100~10)"
     sys.exit()
 
 fil = codecs.open(params[1], encoding="UTF-8")
@@ -37,7 +37,12 @@ for i in params[2:]:
     b = i[1:].split("-", 1)
     b[0] = i[0] + b[0]
     if len(b) > 1:
-	for j in range(int(b[0]), int(b[1]) + 1):
+	step = 1
+	c = b[1].split("~", 1)
+	if len(c) > 1:
+	    b[1] = c[0]
+	    step = int(c[1])
+	for j in range(int(b[0]), int(b[1]) + 1, step):
 	    print >>out, s.run(prefix + str(j))
     else:
 	print >>out, s.run(prefix + i)
