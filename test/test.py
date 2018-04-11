2dff48ece9c83331df7ac5be657886b273cc808d
László Németh <nemeth@numbertext.org>
2018-04-11 14:18:01 +0200

    Extend make check: duplicates and language code dependent lines

--- test/test.py
+++ test/test.py
@@ -1,13 +1,21 @@
+from __future__ import print_function
 import sys
 import Soros
 import codecs
+#
+# test input_file input_stream output_stream [conditional_language_codes...]
+#
 fil = codecs.open(sys.argv[1], encoding="UTF-8")
-inp = codecs.open(sys.argv[2], encoding="UTF-8")
+inp = codecs.open(sys.argv[2], encoding="UTF-8").readlines()
 out = codecs.open(sys.argv[3], "wb", encoding="UTF-8")
-s = Soros.compile(fil.read())
-for i in inp:
-    try:
-        print >>out, s.run(i.strip())
-    except:
-        print(s.run(i.strip()))
+prg = fil.read()
+s = {}
+langs = ["default"] + sys.argv[4:]
+for l in langs:
+    s[l] = Soros.compile(prg, l)
+for l in langs:
+    print("Language: " + l, file=sys.stderr)
+    print("Language: " + l, file=out)
+    for i in inp:
+        print(s[l].run(i.strip()), file=out)
 
