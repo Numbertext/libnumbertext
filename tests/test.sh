f8c2e9866a830b4daee318a6a9b0b18422cdbd45
László Németh <nemeth@numbertext.org>
2022-11-03 17:54:50 +0100

    Revert "[ca, da, es] fix 101000 etc."
    This reverts commit 3e00d6160e795c6733e11466e344e7cd2f671e17
    "[ca, da, es] fix 101000 etc.", according to the fix
    for the missing boundary detection of conditional text
    in commit ecb3d6840d974f41bdd0967c4e2a69615e0669da
    "[Soros] fix right boundary inheritance in conditional text".
    
    test*/es.*: add test for 231231 and 531531, as reported
    by JuanmaSP in GitHub issue #70.

--- tests/test.sh
+++ tests/test.sh
@@ -10,5 +10,5 @@ if [ ! -d $TEMPDIR ]; then
   mkdir $TEMPDIR
 fi
 
-$SPELLOUT -l $TESTDIR/../data/$1 $(grep '^[^#]' $2) >$TEMPDIR/test.out$$
+$SPELLOUT -l $TESTDIR/../data/$1 $(sed -n 's/ #.*$//;/^[^#]/p' $2) >$TEMPDIR/test.out$$
 diff -u $TEMPDIR/test.out$$ $TESTDIR/$3 || exit 1
