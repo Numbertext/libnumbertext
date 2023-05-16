ca572ae91be51ebb78f5b31271f1b8c202e34be7
Codain <Codain@users.noreply.github.com>
2023-05-16 19:39:28 +0000

    Fixing the declaration of Soros::translate

--- src/Soros.cxx
+++ src/Soros.cxx
@@ -198,7 +198,7 @@ void Soros::run(std::wstring& input, int& level, bool begin, bool end)
 
 std::wstring Soros::translate(
         std::wstring s,
-        const std::wstring chars,
+        const std::wstring& chars,
         const std::wstring& chars2,
         const std::wstring& delim)
 {
